5b0a5d78aa8feb3528161da5a50c04c1c5bb2e1e
Carrot <11172391+CRThu@users.noreply.github.com>
2024-02-07 00:42:40 +0800

    flac转换到MP3实现

--- flac2mp3.py
+++ flac2mp3.py
@@ -8,11 +8,18 @@ ffmpeg_dir = os.path.join(".", "ffmpeg", "bin", "ffmpeg.exe")
 # unzip ffmpeg-master-latest-win64-gpl-shared.zip to ./ffmpeg
 # ffmpeg.exe is located at ./ffmpeg/bin
 
-def convert_flac2mp3(fn, bitrate="320k"):
+def convert_flac2mp3(i, fn, bitrate="320k"):
+    print(f"{i + 1}: Convert {fn}.flac to {fn}.mp3")
     cmd = f"{ffmpeg_dir} -i \"{fn}.flac\" -b:a {bitrate} \"{fn}.mp3\""
-    print(cmd)
+    print(f'Command: {cmd}')
     ret = os.system(cmd)
     # if success, return 0
+    if ret == 0:
+        # Check if file exists
+        # Check if file size is not 0
+        if os.path.exists(f'{fn}.mp3') and os.path.getsize(f'{fn}.mp3') > 0:
+            print(f"Deleted file: {fn}.flac")
+            os.remove(f'{fn}.flac')
     return ret
 
 
@@ -23,5 +30,8 @@ def get_flac_filenames(d):
 
 
 flac_list = get_flac_filenames(src_dir)
-for flac_fn in flac_list:
-    convert_flac2mp3(os.path.join(src_dir, flac_fn), "320k")
+
+for idx, flac_fn in enumerate(flac_list):
+    status = convert_flac2mp3(idx, os.path.join(src_dir, flac_fn), "320k")
+    if status != 0:
+        break
